f3567397d693a2dcc87ab36e6946807cd65376cc
Tamas Farkas <tfarkas@2-C-3.42heilbronn.de>
2025-05-10 16:30:51 +0200

    The in, th, ph structures are stack allocated. Fork mutex NULL in the philos_routine.

--- philo/philo_header.h
+++ philo/philo_header.h
@@ -6,7 +6,7 @@
 /*   By: tfarkas <tfarkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 14:04:17 by tfarkas           #+#    #+#             */
-/*   Updated: 2025/05/10 14:03:52 by tfarkas          ###   ########.fr       */
+/*   Updated: 2025/05/10 16:11:44 by tfarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,9 +56,9 @@ typedef struct s_philo
 
 typedef	struct s_coll
 {
-	t_input		*in;
-	t_thread	*th;
-	t_philo		*ph;
+	t_input		in;
+	t_thread	th;
+	t_philo		ph;
 }	t_coll;
 
 
--- philo/philo_main.c
+++ philo/philo_main.c
@@ -6,7 +6,7 @@
 /*   By: tfarkas <tfarkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 14:02:05 by tfarkas           #+#    #+#             */
-/*   Updated: 2025/05/10 14:00:59 by tfarkas          ###   ########.fr       */
+/*   Updated: 2025/05/10 16:26:23 by tfarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,26 +75,16 @@ void	*philos_routine(void *arg)
 
 	coll = (t_coll *)arg;
 	write(1, "hello\n", 6);
-	if (!coll)
-	{
-    	write(2, "Error: coll is NULL\n", 21);
-    	return ((void *)1);
-	}
 
-	if (!(coll->th->fork))
+	if (!coll->th.fork || pthread_mutex_lock(&(coll->th.fork[0])) != 0)
 	{
-    	write(2, "Error: coll->th.fork is NULL\n", 30);
-    	return ((void *)1);
-	}
-	if (pthread_mutex_lock(&(coll->th->fork[0])) != 0)
-	{
-		perror("Failed lock the fork mutex.\n");
+		perror("\033[1;35mFailed lock the fork mutex\033[0m");
 		write(1, "hello3\n", 7);
 		return ((void *)1);
 	}
 	write(1, "hello2\n", 7);
-	printf("The philo[%d] are saying hello to you.\n", coll->ph->philo_id);
-	pthread_mutex_unlock(&(coll->th->fork[0]));
+	printf("The philo[%d] are saying hello to you.\n", coll->ph.philo_id);
+	pthread_mutex_unlock(&(coll->th.fork[0]));
 	return ((void *)0);
 }
 
@@ -111,16 +101,16 @@ int	philo_threads(t_coll *coll)
 	int	i;
 
 	i = 0;
-	coll->th->philo = malloc(coll->in->philo_num * sizeof(pthread_t));
-	if (!coll->th->philo)
+	coll->th.philo = malloc(coll->in.philo_num * sizeof(pthread_t));
+	if (!coll->th.philo)
 	{
 		write_stderr("Malloc failed for philo threads allocation.\n");
 		return (-1);
 	}
-	while (i < coll->in->philo_num)
+	while (i < coll->in.philo_num)
 	{
-		coll->ph->philo_id = i + 1;
-		if (pthread_create(&(coll->th->philo[i]), NULL,
+		coll->ph.philo_id = i + 1;
+		if (pthread_create(&(coll->th.philo[i]), NULL,
 				philos_routine, (void *)coll) != 0)
 		{
 			write_stderr("philo thread creation failed.\n");
@@ -132,7 +122,7 @@ int	philo_threads(t_coll *coll)
 		// 	write_stderr("philo thread creation failed.\n");
 		// 	return (free_memory(&coll->th), -2);
 		// }
-		if (pthread_detach(coll->th->philo[i]) != 0)
+		if (pthread_detach(coll->th.philo[i]) != 0)
 		{
 			write_stderr("philo thread detach failed.\n");
 			// return (free_memory(&coll->th), -2);
@@ -168,7 +158,7 @@ void	*print_monitor(void *arg)
 
 	i = 0;
 	coll = (t_coll *)arg;
-	while (coll->th->start_t < 0)
+	while (coll->th.start_t < 0)
 	{
 		usleep(10);
 		continue ;
@@ -176,30 +166,30 @@ void	*print_monitor(void *arg)
 	sim_start_t = get_current_time();
 	if (sim_start_t < 0)
 		return ((void * )(-1));
-	while (i < coll->in->philo_num)
+	while (i < coll->in.philo_num)
 	{
-		print_message(get_current_time() - coll->th->start_t, i + 1, THINK);
+		print_message(get_current_time() - coll->th.start_t, i + 1, THINK);
 		i++;
 	}
-	if (coll->in->philo_num == 1)
-		one_philo(coll->th->start_t, (long)coll->in->die_t);
+	if (coll->in.philo_num == 1)
+		one_philo(coll->th.start_t, (long)coll->in.die_t);
 	return(NULL);
 }
 
 int	print_thread(t_coll *coll)
 {
-	coll->th->start_t = -1;
-	coll->ph->eat_start_t = -1;
-	coll->th->fork = NULL;
-	if (pthread_create(&(coll->th->monitor), NULL, print_monitor, (void *)coll) != 0)
+	coll->th.start_t = -1;
+	coll->ph.eat_start_t = -1;
+	coll->th.fork = NULL;
+	if (pthread_create(&(coll->th.monitor), NULL, print_monitor, (void *)coll) != 0)
 	{
 		write_stderr("monitor thread creation failed.\n");
 		return (-1);
 	}
 	if (philo_threads(coll) < 0)
 		return (-2);
-	coll->th->start_t = get_current_time();
-	if (coll->th->start_t < 0)
+	coll->th.start_t = get_current_time();
+	if (coll->th.start_t < 0)
 	{
 		write_stderr("Failed to get the start_time");
 		return (-3);
@@ -245,10 +235,10 @@ int	main(int argc, char **argv)
 
 	// pthread_create(&id, NULL, testfunc, NULL);
 	write(1, "passed0\n", 8);
-	if (!check_input(argc, argv, (coll.in)))
+	if (!check_input(argc, argv, &coll.in))
 		return (1);
 	write(1, "passed1\n", 8);
-	if (create_mutexes(coll.th, coll.in->philo_num) < 0)
+	if (create_mutexes(&coll.th, coll.in.philo_num) < 0)
 		return(1);
 	// return(free_memory(&coll.th), 1);
 	write(1, "passed2\n", 8);
@@ -256,14 +246,14 @@ int	main(int argc, char **argv)
 		return (1);
 	// return (free_memory(&coll.th), 1);
 	write(1, "passed3\n", 8);
-	if (pthread_join(coll.th->monitor, NULL) != 0)
+	if (pthread_join(coll.th.monitor, NULL) != 0)
 	{
 		write_stderr("Failed to join the monitor thread.\n");
 		return (1);
 	}
 	write(1, "passed4\n", 8);
 	time_in_ms1 = get_current_time();
-	printf("This is the store input: %d\n", coll.in->die_t);
+	printf("This is the store input: %d\n", coll.in.die_t);
 	printf("The 0.current time is: %ld\n", time_in_ms1);
 	my_usleep(20);
 	time_in_ms2 = get_current_time();
